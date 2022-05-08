1e93d9e636dff41808e0f173bc8da1c844d5644c
ansango <anibalsantosgo@gmail.com>
2022-05-08 14:57:30 +0200

    content layer config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { withContentlayer } = require("next-contentlayer");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+
+module.exports = withContentlayer()({
+  swcMinify: true,
+  reactStrictMode: true,
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+
+    return config;
+  },
+});
