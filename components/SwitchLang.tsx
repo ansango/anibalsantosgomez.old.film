de2360a82df3d4da8735083cb9e30967170a78ef
ansango <anibalsantosgo@gmail.com>
2022-05-11 20:40:38 +0200

    fix

--- components/SwitchLang.tsx
+++ components/SwitchLang.tsx
@@ -3,12 +3,15 @@ import { useRouter } from "next/router";
 import useTranslation from "next-translate/useTranslation";
 const SwitchLang = () => {
   const { asPath, locale } = useRouter();
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
   const newLocale = locale === "en" ? "es" : "en";
+  console.log("asPath", asPath);
+  console.log("locale", locale);
+  console.log("newLocale", newLocale);
   return (
     <button className="uppercase">
       <Link href={asPath} locale={newLocale}>
-        <a>{newLocale}</a>
+        <a>{t(`common:langs.${newLocale}.value`)}</a>
       </Link>
     </button>
   );
