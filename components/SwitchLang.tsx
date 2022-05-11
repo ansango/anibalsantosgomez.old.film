ea90eca56355054355ffdffa90f6027308df097a
ansango <anibalsantosgo@gmail.com>
2022-05-11 20:32:17 +0200

    fix

--- components/SwitchLang.tsx
+++ components/SwitchLang.tsx
@@ -2,26 +2,14 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import useTranslation from "next-translate/useTranslation";
 const SwitchLang = () => {
-  const { asPath, locale, locales } = useRouter();
+  const { asPath, locale } = useRouter();
   const { t } = useTranslation();
-
-  const langsRoutes = locales
-    ?.map((loc: string) => {
-      return {
-        route: asPath,
-        loc,
-        label: t(`common:langs.${loc}.value`),
-      };
-    })
-    .filter(({ loc }) => loc !== locale);
-
+  const newLocale = locale === "en" ? "es" : "en";
   return (
     <button className="uppercase">
-      {langsRoutes.map(({ route, loc, label }) => (
-        <Link href={route} key={loc} locale={loc}>
-          <a>{label}</a>
-        </Link>
-      ))}
+      <Link href={asPath} locale={newLocale}>
+        <a>{t(`common:langs.${newLocale}.value`)}</a>
+      </Link>
     </button>
   );
 };
