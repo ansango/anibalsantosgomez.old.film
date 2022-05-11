f983dcbc564b9223d43fc2d495ea4d9e97753aac
ansango <anibalsantosgo@gmail.com>
2022-05-11 21:11:34 +0200

    err boundary

--- components/SwitchLang.tsx
+++ components/SwitchLang.tsx
@@ -1,20 +1,55 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
+import { Component, ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+class ErrorBoundary extends Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
 
 const SwitchLang = () => {
   const { asPath, locale } = useRouter();
   const { t } = useTranslation();
+
   const newLocale = locale === "en" ? "es" : "en";
-  console.log("asPath", asPath);
-  console.log("locale", locale);
-  console.log("newLocale", newLocale);
+
   return (
-    <button className="uppercase">
-      <Link href={asPath} locale={newLocale}>
-        <a>{t(`common:langs.${newLocale}.value`)}</a>
-      </Link>
-    </button>
+    <ErrorBoundary>
+      <button className="uppercase">
+        <Link href={asPath} locale={newLocale}>
+          <a>{t(`common:langs.${newLocale}.value`)}</a>
+        </Link>
+      </button>
+    </ErrorBoundary>
   );
 };
 
