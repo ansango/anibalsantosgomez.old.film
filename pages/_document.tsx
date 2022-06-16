c2d430b29a5c3abc19b3da1aa08d599c261f45f0
ansango <anibalsantosgo@gmail.com>
2022-06-16 18:44:34 +0200

    favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,34 +11,34 @@ const Document = () => {
           type="font/woff2"
           crossOrigin="anonymous"
         />
-        <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
-        <link href="/static/favicons/site.webmanifest" rel="manifest" />
+        <link href="/favicons/favicon.ico" rel="shortcut icon" />
+        <link href="/favicons/site.webmanifest" rel="manifest" />
         <link
-          href="/static/favicons/apple-touch-icon.png"
+          href="/favicons/apple-touch-icon.png"
           rel="apple-touch-icon"
           sizes="180x180"
         />
         <link
-          href="/static/favicons/favicon-32x32.png"
+          href="/favicons/favicon-32x32.png"
           rel="icon"
           sizes="32x32"
           type="image/png"
         />
         <link
-          href="/static/favicons/favicon-16x16.png"
+          href="/favicons/favicon-16x16.png"
           rel="icon"
           sizes="16x16"
           type="image/png"
         />
         <link
           color="#4a9885"
-          href="/static/favicons/safari-pinned-tab.svg"
+          href="/favicons/safari-pinned-tab.svg"
           rel="mask-icon"
         />
         <meta content="#ffffff" name="theme-color" />
         <meta content="#ffffff" name="msapplication-TileColor" />
         <meta
-          content="/static/favicons/browserconfig.xml"
+          content="/favicons/browserconfig.xml"
           name="msapplication-config"
         />
         <meta
