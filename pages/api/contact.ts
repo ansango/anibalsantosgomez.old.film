5ddc65f87ab281540c616c07893c16b693abedda
ansango <anibalsantosgo@gmail.com>
2022-06-17 20:15:39 +0200

    nodemailer

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
+import { sendMail, CONFIG as MAIL_CONFIG } from "lib/mail";
 import nc from "next-connect";
 
 const handler = nc<NextApiRequest, NextApiResponse>();
@@ -16,6 +16,35 @@ handler.post(async (req, res) => {
   ) {
     return res.status(400).json({ message: "Invalid request" });
   } else {
+    await sendMail({
+      to: MAIL_CONFIG.from,
+      from: MAIL_CONFIG.from,
+      subject: "Contact form submission",
+      html: `
+        <p>
+          Name: ${contactForm.name}
+        </p>
+        <p>
+          Email: ${contactForm.email}
+        </p>
+        <p>
+
+          Message: ${contactForm.message}
+        </p>
+      `,
+    });
+    await sendMail({
+      to: contactForm.email,
+      from: MAIL_CONFIG.from,
+      subject: "Contact form",
+      html: `
+        <h1>Hola ${contactForm.name}</h1>
+        <p>
+          Gracias por contactarnos.
+          En breve nos comunicaremos con vos.
+        </p>
+      `,
+    });
     return res.status(200).json({ message: "Success" });
   }
 });
