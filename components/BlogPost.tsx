d02863e7a4d59cc9cf77d2018d2bd580f14f3fec
Aníbal Santos Gómez <anibalsantosgo@gmail.com>
2022-09-06 02:37:57 +0200

    Update BlogPost.tsx

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -21,7 +21,7 @@ const BlogPost = ({
       <a className="w-full flex space-y-5 mb-2">
         <li className="w-full mb-2 lowercase">
           <div className="flex justify-between flex-row">
-            <h4 className="w-full text-lg font-medium text-gray-900">
+            <h4 className="w-full text-lg font-medium text-gray-900 line-clamp-1">
               {title}
             </h4>
             <p className="mb-4 text-left text-gray-500 flex items-center space-x-1 w-full justify-end">
