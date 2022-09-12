53b7a54250667d042eaac349b1416f9132465e2a
ansango <anibalsantosgo@gmail.com>
2022-09-13 00:03:15 +0200

    refactor and reescaning

--- scripts/transform-images.mjs
+++ scripts/transform-images.mjs
@@ -1,26 +1,33 @@
 import sharp from "sharp";
 import { globby } from "globby";
-import { readFileSync, mkdirSync, existsSync, rmdirSync } from "fs";
+import { readFileSync, mkdirSync, existsSync, rmdirSync, copyFile } from "fs";
 
-const transformImages = async (src, format) => {
+const transformImages = async (src, format, skip = true) => {
   const images = await globby(src);
+
   for (const image of images) {
     const folder = image.replace("./blog/", "").split("/")[0];
     const outputFolder = `./public/blog/${folder}`;
     const imageName = image.split("/").pop().split(".")[0];
     const output = `${outputFolder}/${imageName}.${format}`;
     const imageBuffer = await readFileSync(image);
-    const imageSharp = sharp(imageBuffer)
-      .resize({ width: 1152, height: 768 })
-      .toFormat("webp")
-      .webp({ quality: 80 });
-
-    if (!existsSync(outputFolder)) {
-      mkdirSync(outputFolder);
+    if (!skip) {
+      if (!existsSync(outputFolder)) {
+        mkdirSync(outputFolder);
+      }
+      await sharp(imageBuffer)
+        .resize({ width: 1152, height: 768 })
+        .toFormat("webp")
+        .webp({ quality: 80 })
+        .toFile(output);
+      console.log(`${image} -> ${output}`);
+    } else {
+      if (!existsSync(outputFolder)) {
+        mkdirSync(outputFolder);
+      }
+      await sharp(imageBuffer).toFile(output);
+      console.log(`${image} -> ${output}`);
     }
-
-    await imageSharp.toFile(output);
-    console.log(`${image} -> ${output}`);
   }
   for (const image of images) {
     const baseFolder = image.split("/").slice(0, -1).join("/");
@@ -31,4 +38,4 @@ const transformImages = async (src, format) => {
   console.log("Done");
 };
 
-transformImages("./blog/**/images/*.{jpg,JPG,jpeg,JPEG,png,gif}", "webp");
+transformImages("./blog/**/images/*.{jpg,JPG,jpeg,JPEG,png,gif,webp}", "webp");
