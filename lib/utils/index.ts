9c79a5b9a72361ccf7b459803c16c382d74604f2
ansango <anibalsantosgo@gmail.com>
2022-09-20 19:34:19 +0200

    fetcher

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -42,3 +42,31 @@ export const countPhotos = (serie: any) => {
       : "";
   return result;
 };
+
+
+export type ErrorFetcher = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  error: any;
+  message: string;
+};
+
+const fetcher = async (
+  ...args: Parameters<typeof fetch>
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+): Promise<any | Error> => {
+  return fetch(...args)
+    .then()
+    .then((response) => {
+      if (response.status === 204) {
+        return Promise.resolve();
+      } else if (!response.ok) {
+        return Promise.reject({
+          message: response.statusText,
+          error: response.status,
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => Promise.reject(error));
+};
+export default fetcher;
