f0ab8c793490403df196597394966293e59f1e76
ansango <anibalsantosgo@gmail.com>
2022-09-20 11:45:56 +0200

    404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,23 @@
 import { Layout } from "../components/layout";
+import { Container } from "../components/util/container";
 
 export default function FourOhFour() {
-  return <Layout>OOOOPSSS</Layout>;
+  return (
+    <Layout>
+      <Container>
+        <section>
+          <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 md:px-12 lg:px-24 lg:py-24">
+            <div className="flex flex-col w-full mb-12 text-center">
+              <h1 className="max-w-5xl text-2xl font-bold leading-none tracking-tighter text-neutral-600 md:text-5xl lg:text-6xl lg:max-w-7xl">
+                404 - Page Not Found
+              </h1>
+              <p className="max-w-xl mx-auto mt-8 text-base leading-relaxed text-center text-gray-500">
+                The page you are looking for does not exist.
+              </p>
+            </div>
+          </div>
+        </section>
+      </Container>
+    </Layout>
+  );
 }
