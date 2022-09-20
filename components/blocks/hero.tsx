f6dbd04568691ccfa5397663a0c1ed56fcb346bc
ansango <anibalsantosgo@gmail.com>
2022-09-20 13:21:44 +0200

    hero

--- components/blocks/hero.tsx
+++ components/blocks/hero.tsx
@@ -6,58 +6,89 @@ import { useTheme } from "../layout";
 import type { TinaTemplate } from "tinacms";
 import Image from "next/image";
 
-const titleColors = {
-  slate: "text-slate-600 dark:text-slate-50",
-  gray: "text-gray-600 dark:text-gray-50",
-  zinc: "text-zinc-600 dark:text-zinc-50",
-  neutral: "text-neutral-600 dark:text-neutral-50",
-  stone: "text-stone-600 dark:text-stone-50",
+const renderHero = (data, parentField) => {
+  switch (data.type) {
+    case "left":
+      return (
+        <Section color={data.color}>
+          <Container>
+            <div className="flex flex-wrap items-center mx-auto 2xl:max-w-7xl">
+              <div className="flex flex-col items-start mb-16 text-left lg:flex-grow lg:w-1/2 lg:pr-24 md:mb-0">
+                {data.tagline && (
+                  <span
+                    className="mb-8 text-xs font-bold tracking-widest text-blue-600 uppercase"
+                    data-tinafield={`${parentField}.tagline`}
+                  >
+                    {data.tagline}
+                  </span>
+                )}
+                {data.headline && (
+                  <h1
+                    className="mb-8 text-4xl font-bold leading-none tracking-tighter text-neutral-600 md:text-7xl"
+                    data-tinafield={`${parentField}.headline`}
+                  >
+                    {data.headline}
+                  </h1>
+                )}
+                {data.text && (
+                  <p
+                    className="mb-8 text-base leading-relaxed text-left text-gray-500"
+                    data-tinafield={`${parentField}.text`}
+                  >
+                    {data.text}
+                  </p>
+                )}
+              </div>
+            </div>
+          </Container>
+          {data.image?.src && (
+            <Container>
+              <div
+                className="flex flex-col items-center justify-center"
+                data-tinafield={`${parentField}.image`}
+              >
+                <Image
+                  className="object-cover object-center w-full"
+                  alt={data.image.alt}
+                  src={data.image.src ?? ""}
+                  width={data.image.type === "portrait" ? 1365 : 2048}
+                  height={data.image.type === "portrait" ? 2048 : 1365}
+                />
+              </div>
+            </Container>
+          )}
+        </Section>
+      );
+    case "center":
+      return (
+        <Section color={data.color}>
+          <Container>
+            <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 md:px-12 lg:px-24 lg:py-24">
+              <div className="flex flex-col w-full mb-12 text-center">
+                {data.headline && (
+                  <h1 className="max-w-5xl text-2xl font-bold leading-none tracking-tighter text-neutral-600 md:text-5xl lg:text-6xl lg:max-w-7xl" data-tinafield={`${parentField}.headline`}>
+                    {data.headline}
+                  </h1>
+                )}
+                {data.text && (
+                  <p className="max-w-xl mx-auto mt-8 text-base leading-relaxed text-center text-gray-500"  data-tinafield={`${parentField}.text`}>
+                    {data.text}
+                  </p>
+                )}
+              </div>
+            </div>
+          </Container>
+        </Section>
+      );
+    default:
+      return null;
+  }
 };
 
 export const Hero = ({ data, parentField = "" }) => {
   const theme = useTheme();
 
-  return (
-    <Section color={data.color}>
-      <Container>
-        <div className="flex flex-wrap items-center mx-auto 2xl:max-w-7xl">
-          <div className="flex flex-col items-start mb-16 text-left lg:flex-grow lg:w-1/2 lg:pr-24 md:mb-0">
-            {data.tagline && (
-              <span className="mb-8 text-xs font-bold tracking-widest text-blue-600 uppercase">
-                {data.tagline}
-              </span>
-            )}
-            {data.headline && (
-              <h1 className="mb-8 text-4xl font-bold leading-none tracking-tighter text-neutral-600 md:text-7xl">
-                {data.headline}
-              </h1>
-            )}
-            {data.text && (
-              <p className="mb-8 text-base leading-relaxed text-left text-gray-500">
-                {data.text}
-              </p>
-            )}
-          </div>
-        </div>
-      </Container>
-      {data.image?.src && (
-        <Container>
-          <div
-            className="flex flex-col items-center justify-center"
-            data-tinafield={`${parentField}.image`}
-          >
-            <Image
-              className="object-cover object-center w-full"
-              alt={data.image.alt}
-              src={data.image.src ?? ""}
-              width={data.image.type === "portrait" ? 1365 : 2048}
-              height={data.image.type === "portrait" ? 2048 : 1365}
-            />
-          </div>
-        </Container>
-      )}
-    </Section>
-  );
+  return <>{renderHero(data, parentField)}</>;
 };
 
 export const heroBlockSchema: TinaTemplate = {
@@ -69,6 +100,7 @@ export const heroBlockSchema: TinaTemplate = {
       tagline: "Your tagline",
       headline: "This Big Text is Totally Awesome",
       text: "Here's some text above the other text",
+      type: "left",
     },
   },
   fields: [
@@ -89,6 +121,12 @@ export const heroBlockSchema: TinaTemplate = {
       ui: {
         component: "textarea",
       },
+    },
+    {
+      type: "string",
+      label: "Type",
+      name: "type",
+      options: ["left", "center"],
     },
     {
       type: "object",
--- components/util/container.tsx
+++ components/util/container.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-export const Container = ({ children, className = "" }) => {
+export const Container = ({ children, className = "", ...props }) => {
   return (
     <div
       className={`w-full max-w-7xl 2xl:max-w-7xl sm:px-6 md:px-12 lg:px-24  2xl:px-12 px-4 py-12 lg:py-24 mx-auto ${className}`}
+      {...props}
     >
       {children}
     </div>
