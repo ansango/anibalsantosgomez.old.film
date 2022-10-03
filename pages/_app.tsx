397a7aa4afb2930e8c8c3874190af9a9ab7b00f4
ansango <anibalsantosgo@gmail.com>
2022-10-03 15:50:59 +0200

    Google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles.css";
 import TinaProvider from "../.tina/components/TinaDynamicProvider";
 import NextHead from "next/head";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 const App = ({ Component, pageProps }) => {
   return (
@@ -8,10 +9,9 @@ const App = ({ Component, pageProps }) => {
       <NextHead>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </NextHead>
-
+      <GoogleAnalytics trackPageViews />
       <TinaProvider>
         <Component {...pageProps} />
-        
       </TinaProvider>
     </>
   );
