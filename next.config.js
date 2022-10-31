7f035d9c5cb07d8600cc6c7f68ae1abe6d12dc62
ansango <anibalsantosgo@gmail.com>
2022-10-31 18:40:30 +0100

    rev

--- next.config.js
+++ next.config.js
@@ -1,19 +1,15 @@
 const withSvgr = require("next-svgr");
 
-module.exports = withSvgr(
-  {
-    images: {
-      domains: ["asg-cms.s3.eu-west-3.amazonaws.com"],
-    },
+module.exports = withSvgr({
+  images: {
+    domains: ["asg-cms.s3.eu-west-3.amazonaws.com"],
   },
-  {
-    async rewrites() {
-      return [
-        {
-          source: "/",
-          destination: "/home",
-        },
-      ];
-    },
-  }
-);
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+      },
+    ];
+  },
+});
